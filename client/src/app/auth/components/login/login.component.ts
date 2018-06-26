2e681b6cb647743ad540d10a311f433fe3cab13e
Franklin E. Parker <fparker@deliveryman.com>
2018-06-25 21:06:14 -0400

    login

--- client/src/app/auth/components/login/login.component.ts
+++ client/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isLoading = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onLogin(form: NgForm){
+    console.log('form',form.value);
   }
 
 }
