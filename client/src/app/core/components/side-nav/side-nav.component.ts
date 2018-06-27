e791e68d99d30d54311f88394585d406f214b783
Franklin E. Parker <fparker@deliveryman.com>
2018-06-27 18:14:25 -0400

    fix display menu items

--- client/src/app/core/components/side-nav/side-nav.component.ts
+++ client/src/app/core/components/side-nav/side-nav.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, OnDestroy} from '@angular/core';
 import {AuthService} from "../../../auth/service/auth.service";
+import {LoggedInUser} from "../../../auth/model/loggedInUser";
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent implements OnInit {
+export class SideNavComponent implements OnInit, OnDestroy {
   @Output('closeSideNav') closeSideNav = new EventEmitter();
+  userLoggedIn: boolean;
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getUserLoggedInUserObservable()
+      .subscribe((loggedInUser:LoggedInUser)=>{
+        this.userLoggedIn = !!loggedInUser;
+        console.log('userLoggedIn:' + this.userLoggedIn);
+      });
+
   }
+
+  ngOnDestroy(){
+    console.log('side nav destroyed');
+  }
+
 
   onSidenavClose(){
     this.closeSideNav.emit();
